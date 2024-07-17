dc258feed3ef8bbd6d8c4e436c28b02c8cb60e37
Skrla <skrlama@gmail.com>
2024-07-17 20:51:14 +0200

    Updated modal background and text

--- snake-client/src/components/modals/GameOverModal.tsx
+++ snake-client/src/components/modals/GameOverModal.tsx
@@ -20,16 +20,19 @@ const GameOverModal = ({ score, handlePlayAgain, handleClose, connection }: Game
     };
 
     return (
-        <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full">
-            <div className="bg-dark-600 border-dark-500 p-5 text-center rounded-xl shadow-2xl w-full max-w-[380px]">
-                <h2 className="text-green-500 text-xl">GAME OVER</h2>
-                <p className="text-white-500">Your Score: {score}</p>
-                <div className="max-w-[400px] w-full flex my-4 justify-center items-center gap-4">
-                    <ModalButton text="Play Again" onClick={playAgain} />
-                    <ModalButton text="Take screenshot" onClick={closeModal} secondary />
+        <>
+            <div className="fixed top-0 left-0 w-screen h-screen bg-dark-500 opacity-65"></div>
+            <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full">
+                <div className="bg-dark-600 border-dark-500 p-5 text-center rounded-xl shadow-2xl w-full max-w-[380px]">
+                    <h2 className="text-green-500 text-xl">GAME OVER</h2>
+                    <p className="text-white-500">Your Score: {score}</p>
+                    <div className="max-w-[400px] w-full flex my-4 justify-center items-center gap-4">
+                        <ModalButton text="Play Again" onClick={playAgain} />
+                        <ModalButton text="Take screenshot" onClick={closeModal} secondary />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- snake-client/src/components/modals/GameStartModal.tsx
+++ snake-client/src/components/modals/GameStartModal.tsx
@@ -37,43 +37,44 @@ const GameStartModal = ({ startGame, startGameTest }: GameStartModalProps) => {
     }
 
     return (
-        <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full">
-            <div className="flex flex-col items-center justify-center bg-white p-5 mx-5 text-center rounded-lg w-[35%] min-w-96 max-w-[700px]">
-                <div className="flex flex-col py-3 max-w-[380px] w-full px-6 gap-3 text-white-500 bg-dark-600 rounded-md shadow-xlg">
-                    <h2 className="text-xl text-green-500">Play Blink Snake</h2>
-                    {loading ? (
-                        <>
-                            <h3 className="text-lg text-center"> GAME STARTING IN:</h3>
-                            <p className="text-6xl text-center font-bold">{timer}</p>
-                        </>
-                    ) : (
-                        <>
-                            <p className="flex flex-col gap-3 text-start text-white-500">
-                                Blink Snake is a game on Solana blockchain. You can play it for sol and if you get the
-                                new record you get all the accumulated money that is used for game fee.
-                            </p>
-                            <hr />
-                            <p className="flex flex-col gap-3 text-start text-white-500">
-                                To play, use your arrow keys on your keyboard. Use arrow buttons if on mobile.
-                            </p>
-                            <hr />
-                            <ModalButton
-                                onClick={handleButton}
-                                text={`${
-                                    connection && wallet
-                                        ? 'Play Game For 0.01'
-                                        : 'Connect Your Wallet To Play For Prizes'
-                                }`}
-                                className="flex justify-center items-center"
-                                visible={connection && wallet ? true : false}
-                            />
-                            <ModalButton onClick={handleButtonTest} text="Play Test Game" />
-                        </>
-                    )}
-                    
+        <>
+            <div className="fixed top-0 left-0 w-screen h-screen bg-dark-500 opacity-65"></div>
+            <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full z-10">
+                <div className="flex flex-col items-center justify-center bg-white p-5 mx-5 text-center mt-10 rounded-lg w-[35%] min-w-96 max-w-[700px]">
+                    <div className="flex flex-col py-6 max-w-[380px] w-full px-9 gap-3 text-white-500 bg-dark-600 rounded-md shadow-xlg">
+                        <h2 className="text-2xl	font-semibold text-green-500">Play Blink Snake</h2>
+                        {loading ? (
+                            <>
+                                <h3 className="text-lg text-center"> GAME STARTING IN:</h3>
+                                <p className="text-6xl text-center font-bold">{timer}</p>
+                            </>
+                        ) : (
+                            <>
+                                <p className="flex flex-col gap-3 text-start text-white-500">
+                                    Blink Snake is a game on the Solana blockchain. You can play it for SOL, and if you
+                                    set a new record, you receive all the accumulated money from the game fees.
+                                </p>
+                                <hr />
+                                <p className="flex flex-col gap-3 text-start text-white-500 mb-8">
+                                    To play, use your arrow keys on your keyboard. Use arrow buttons if on mobile.
+                                    <hr />
+                                </p>
+                                <p className="flex flex-col gap-3 text-center font-semibold text-white-500 text-lg">
+                                    Connect Wallet To Play For Prizes
+                                </p>
+                                <ModalButton
+                                    onClick={handleButton}
+                                    text={`${connection && wallet ? 'Play Game For 0.01' : 'Connect Wallet'}`}
+                                    className="flex justify-center items-center bg-purple-500"
+                                    visible={connection && wallet ? true : false}
+                                />
+                                <ModalButton onClick={handleButtonTest} text="Play Test Game" />
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
